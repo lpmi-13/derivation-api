4fc65d6af20d65b0194e6da5eca713dbf954c8fc
lpmi-13 <leskis@gmail.com>
2018-06-05 06:51:06 +0100

    update response to json

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   app.get('/derivations', function(req, res) {
     const term = req.query.word;
     const results = keySearch({ term });
+    res.setHeader('Content-Type', 'application/json');
     res.send(results);
   });
 }
