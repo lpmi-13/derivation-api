11afeb59c4ded54b56aeb2252e3c50517fff5205
lpmi-13 <leskis@gmail.com>
2018-06-10 22:06:10 +0100

    simply response

--- router.js
+++ router.js
@@ -1,14 +1,13 @@
 const Word = require('./models/word');
 
 module.exports = function(app) {
-  app.get('/derivations', function(req, res) {
-    const term = req.query.word;
-    Word.findOne({ word: term}, function(err, obj) {
-      if (obj === null) {
-        res.send({"message": 'nothing found'}); 
-      } else {
-        res.setHeader('Content-Type', 'application/json');
+  app.get('/', function(req, res) {
+    Word.findOne({ word: req.query.word}, function(err, obj) {
+      res.setHeader('Content-Type', 'application/json');
+      if (obj) { 
         res.send(obj.word_list);
+      } else {
+        res.send({'message': 'nothing found'}); 
       }
     });
   });
